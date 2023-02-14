ec2467df2bb0ae58dcf2c9650058746ee5e8dc08
lfederico <luigi.federico.rossi@gmail.com>
2023-02-14 16:27:28 +0000

    comments

--- +mfun/sizeTun.m
+++ +mfun/sizeTun.m
@@ -1,4 +1,6 @@
 function st_r = sizeTun(pars, stim_size)
+% modified from Dipoppa et al., Neuron, 2018 
+
 
 % f(s) = R*[erf(s/sigma1 - k*erf(s/sigma2)];
 
@@ -6,8 +8,8 @@ R = pars(1); % amplitude of response
 sigma1 = pars(2); % standard deviation 1 in degrees
 sigma2 = pars(3); % standard deviation 1 in degrees
 b = pars(4); % baseline
-k = pars(5); % not sure what this is for
-s = stim_size; % stimulus size in degrees
+k = pars(5); % allows to switch the sign of the subtraction. Besides the sign, I think it trades off with the sigmas
+s = stim_size; % stimulus ssize in degrees
 
 st_r = R*(erf(s/sigma1.^2) - k*erf(s/sigma2.^2)) +b;
 
